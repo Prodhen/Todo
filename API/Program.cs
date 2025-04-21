9be05c020af6b09f3d67b00feaa9d176094bae25
Arosh Prodhen <arosh.prodhen@technonext.com>
2025-04-21 14:57:02 +0600

    SerilogAndSwaggerAdded

--- API/Program.cs
+++ API/Program.cs
@@ -8,15 +8,37 @@ using FluentValidation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger(); // <-- Temporary logger for startup
+
+// Setup Serilog from configuration
+builder.Host.UseSerilog((context, services, configuration) =>
+{
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext();
+});
+
 
 // Add services to the container.
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.Register();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer(); // <-- Required for Swagger to see endpoints
+builder.Services.AddSwaggerGen();           // <-- Generates Swagger UI and JSON
 var app = builder.Build();
+
+
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 // Configure the HTTP request pipeline.
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.UseStaticFiles();
